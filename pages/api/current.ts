e661a05529841a9ca8a97ff9b1a0b44601924426
Satria-WS <extencezone02@gmail.com>
2024-04-07 23:50:41 +0700

    fixing deploy

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,26 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import serverAuth from "@/lib/serverAuth"; // Importing the serverAuth function for authentication
+import serverAuth from "@/lib/serverAuth";
 
-// Define the request handler function
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // Check if the request method is not GET
-    if (req.method !== "GET") {
-      // If it's not a GET request, return a 405 Method Not Allowed status code
+    if (req.method !== 'GET') {
       return res.status(405).end();
     }
 
-    // Authenticate the user using the serverAuth function
     const { currentUser } = await serverAuth(req);
 
-    // If authentication is successful, return the current user as JSON
     return res.status(200).json(currentUser);
   } catch (error) {
-    // If an error occurs during authentication or processing, log the error and return a 500 Internal Server Error status code
     console.log(error);
     return res.status(500).end();
   }
-};
-
-// Export the request handler function as the default export of this module
-export default handler;
+}
