087eca3f2ce643ba608cdf2cfc718a50005970a5
Satria-WS <extencezone02@gmail.com>
2024-04-08 12:07:33 +0700

    add

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,17 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
+const client = global.prismadb || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prismadb = client
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
-  }
-  prisma = globalWithPrisma.prisma;
-}
-
-export default prisma;
+export default client
